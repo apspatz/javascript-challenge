2863fbba9aae1a158fa8af65e6fd8aaef26175d7
unknown <andrew.p.spatz@gmail.com>
2019-11-22 08:39:27 -0500

    level 1 complete

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -5,7 +5,7 @@ var tableData = data;
 // Inputting Table Data
 var table = d3.select("#ufo-table");
 console.log(table);
-var tbody = d3.select(table.tbody);
+var tbody = d3.select("tbody");
 console.log(tbody);
 tableData.forEach(sighting =>{
     var row = tbody.append("tr");
@@ -23,9 +23,11 @@ var textbox = d3.select("#datetime");
 button.on("click", function(){
     console.log("Filter button clicked");
     var entry = textbox.property("value");
-    if(tableData.datetime === entry){
-        var filteredData = tableData.filter(entry => tableData.datetime === entry);
-        tbody.html('');
+    console.log(entry);
+    var filteredData = tableData.filter(date => date.datetime === entry);
+    console.log(filteredData);
+    tbody.html('');
+    if(filteredData[0]){
         filteredData.forEach(sighting =>{
             var row = tbody.append("tr");
             Object.entries(sighting).forEach(([key, value]) => {
